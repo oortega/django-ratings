02d5727a17a38e61abfa68280573fe5d39386319
bung87 <zh.bung@gmail.com>
2016-04-04 01:58:14 +0800

    compati

--- djangoratings/models.py
+++ djangoratings/models.py
@@ -2,8 +2,13 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
 from django.contrib.auth.models import User
+import django
+if django.version > 1.8:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+else:
+    from django.contrib.contenttypes.generic import GenericForeignKey
+    
 
 try:
     from django.utils.timezone import now
