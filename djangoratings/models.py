21589970adc1c7dbd2be35ebb4cedc81b8d3825c
oortega <oscarortega@outlook.com>
2016-06-18 12:20:27 -0500

    Cannot import name generic Django 1.9

--- djangoratings/models.py
+++ djangoratings/models.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes import fields
+
 from django.contrib.auth.models import User
 
 try:
@@ -26,7 +27,7 @@ class Vote(models.Model):
 
     objects = VoteManager()
 
-    content_object = generic.GenericForeignKey()
+    content_object = fields.GenericForeignKey()
 
     class Meta:
         unique_together = (('content_type', 'object_id', 'key', 'user', 'ip_address', 'cookie'))
@@ -57,7 +58,7 @@ class Score(models.Model):
     score           = models.IntegerField()
     votes           = models.PositiveIntegerField()
     
-    content_object  = generic.GenericForeignKey()
+    content_object  = fields.GenericForeignKey()
 
     class Meta:
         unique_together = (('content_type', 'object_id', 'key'),)
@@ -85,7 +86,7 @@ class IgnoredObject(models.Model):
     content_type    = models.ForeignKey(ContentType)
     object_id       = models.PositiveIntegerField()
     
-    content_object  = generic.GenericForeignKey()
+    content_object  = fields.GenericForeignKey()
     
     class Meta:
         unique_together = (('content_type', 'object_id'),)
